bb3a53bcc8726f341662b3b2a2622eb534e97249
Sho DOUHASHI <douhashi@gmail.com>
2025-05-27 16:51:59 +0000

    kamal settings

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,30 +1,29 @@
-require 'capybara/rspec'
 require 'selenium-webdriver'
 
-Capybara.register_driver :chrome_headless do |app|
-  url = ENV['SELENIUM_DRIVER_URL']
-  browser = url ? :remote : :chrome
-
+Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
   options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--headless')
   options.add_argument('--disable-gpu')
-  options.add_argument('--window-size=1400,1400')
-  # To specify the browser version, assign the version number as a string.
-  options.browser_version = nil
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1680,1050')
 
-  Capybara::Selenium::Driver.new(app, browser: browser, url: url, options: options)
+  url = ENV.fetch("SELENIUM_URL", "http://localhost:4444/wd/hub")
+
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, capabilities: options)
 end
+
+Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+Capybara.server_port = 5555
+Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
 
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.before(:each, type: :system) do
-    if ENV['SELENIUM_DRIVER_URL'].present?
-      Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-      Capybara.app_host = "http://#{Capybara.server_host}"
-    end
+    driven_by :chrome
+  end
 
-    driven_by :chrome_headless
+  config.before(:each, type: :system, js: true) do
+    driven_by :chrome
   end
 end
